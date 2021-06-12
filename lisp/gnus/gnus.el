247080e8825a7f933848cd02d974f2d45c73f998
dickmao <none>
2021-06-12 09:43:28 -0400

    more race bait (the synchronicity kind, not the social)

--- lisp/gnus/gnus.el
+++ lisp/gnus/gnus.el
@@ -2465,7 +2465,7 @@ such as a mark that says whether an article is stored in the cache
   "Alist of major modes and related Info nodes.")
 
 (defvar-local gnus-summary-buffer nil)
-(defvar-local gnus-article-buffer nil)
+(defvar-local gnus-article-buffer "*Article*")
 (defvar gnus-server-buffer "*Server*")
 
 (defvar gnus-child nil
