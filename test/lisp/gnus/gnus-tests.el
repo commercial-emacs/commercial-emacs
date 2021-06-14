aa380e7092cc67c2099e684135b2dadbfef10021
dickmao <none>
2021-06-13 20:14:51 -0400

    really try darndest not to trigger debugger in thread

--- test/lisp/gnus/gnus-tests.el
+++ test/lisp/gnus/gnus-tests.el
@@ -28,6 +28,7 @@
 ;;
 
 (require 'cl-macs)
+
 (defmacro gnus-tests-let-customs (bindings &rest forms)
   (declare (indent defun))
   `(let (,@(mapcar #'car bindings))
@@ -38,51 +39,19 @@
 (cl-defmacro gnus-tests-doit (&rest body &key select-methods &allow-other-keys)
   (declare (indent defun))
   `(unwind-protect
-       (let* ((load-file-name "/home/dick/emacs")
-	      (default-directory elpaso-defs-toplevel-dir)
-	      (user-emacs-directory default-directory)
-	      (package-user-dir (locate-user-emacs-file "elpa"))
-	      use-package-ensure-function
-	      elpaso-admin--cookbooks-alist
-	      elpaso-defs-install-dir
-	      elpaso-admin-cookbooks
-	      package-alist
-	      package-activated-list
-	      package-archives
-	      package-archive-contents
-	      (package-directory-list
-	       (eval (car (get 'package-directory-list 'standard-value))))
-	      (package-load-list
-	       (eval (car (get 'package-load-list 'standard-value))))
-	      (package-gnupghome-dir (expand-file-name "gnupg" package-user-dir)))
+       (let* ((default-directory (if-let ((lib (locate-library "gnus-tests")))
+                                     (file-name-directory lib)
+                                   default-directory))
+	      (user-emacs-directory default-directory))
          (gnus-tests-let-customs
            ((gnus-verbose 8)
             (gnus-save-newsrc-file nil)
-            (gnus-home-directory (file-name-directory load-file-name))
+            (gnus-home-directory default-directory)
             (gnus-use-dribble-file nil)
             (network-security-level (quote low))
             (gnus-interactive-exit (quote quiet))
             (gnus-select-methods (quote ((nnfolder "")))))
-           gnus-home-directory)
-
-	 (customize-set-variable 'elpaso-defs-install-dir (locate-user-emacs-file "elpaso"))
-	 (customize-set-variable 'use-package-ensure-function
-				 'elpaso-use-package-ensure-function)
-	 (delete-directory package-user-dir t)
-	 (make-directory package-user-dir t)
-	 (delete-directory elpaso-admin--build-dir t)
-	 (delete-directory elpaso-admin--recipes-dir t)
-	 (customize-set-variable 'elpaso-admin--cookbooks-alist
-				 '((user :file "recipes")
-				   (rtest :url "mockhub.com/recipes.git" :file "recipes")))
-	 (customize-set-variable 'elpaso-admin-cookbooks '(user rtest))
-	 (elpaso-refresh)
-	 (should (file-readable-p
-		  (elpaso-admin--sling elpaso-admin--recipes-dir "rtest/recipes")))
-	 (dolist (spec ,specs)
-	   (elpaso-admin-add-recipe (intern (car spec)) (cdr spec))
-	   (should (member spec (elpaso-admin--get-specs))))
-	 (progn ,@body))
+	   (progn ,@body)))
      t))
 
 (ert-deftest gnus-test-stub () (should t))
