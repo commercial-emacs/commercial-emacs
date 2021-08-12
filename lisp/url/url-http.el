6cab0059f391c9fa7daf595418a4d05e9acbd09a
dickmao <none>
2021-08-12 16:28:00 -0400

    no retry

--- lisp/url/url-http.el
+++ lisp/url/url-http.el
@@ -46,7 +46,6 @@
 (defvar url-http-extra-headers)
 (defvar url-http-noninteractive)
 (defvar url-http-method)
-(defvar url-http-no-retry)
 (defvar url-http-process)
 (defvar url-http-proxy)
 (defvar url-http-response-status)
@@ -1000,30 +999,15 @@ should be shown to the user."
   (url-http-debug "url-http-end-of-document-sentinel in buffer (%s)"
 		  (process-buffer proc))
   (url-http-idle-sentinel proc why)
-  (when (buffer-name (process-buffer proc))
+  (when (buffer-live-p (process-buffer proc))
     (with-current-buffer (process-buffer proc)
       (goto-char (point-min))
-      (cond ((not (looking-at "HTTP/"))
-	     (if url-http-no-retry
-		 ;; HTTP/0.9 just gets passed back no matter what
-		 (url-http-activate-callback)
-	       ;; Call `url-http' again if our connection expired.
-	       (erase-buffer)
-               (let ((url-request-method url-http-method)
-                     (url-request-extra-headers url-http-extra-headers)
-                     (url-request-data url-http-data)
-                     (url-using-proxy (url-find-proxy-for-url
-                                       url-current-object
-                                       (url-host url-current-object))))
-                 (when url-using-proxy
-                   (setq url-using-proxy
-                         (url-generic-parse-url url-using-proxy)))
-                 (url-http url-current-object url-callback-function
-                           url-callback-arguments (current-buffer)
-                           (and (string= "https" (url-type url-current-object))
-                                'tls)))))
-	    ((url-http-parse-headers)
-	     (url-http-activate-callback))))))
+      (if (looking-at "HTTP/")
+          (url-http-parse-headers)
+        (url-http-debug "url-http-end-of-document-sentinel wtf %s\n%s"
+                        why
+		        (buffer-substring-no-properties (point-min) (point-max))))
+      (url-http-activate-callback))))
 
 (defun url-http-simple-after-change-function (_st _nd _length)
   ;; Function used when we do NOT know how long the document is going to be
@@ -1349,7 +1333,6 @@ The return value of this function is the retrieval buffer."
 		       url-http-noninteractive
 		       url-http-data
 		       url-http-target-url
-		       url-http-no-retry
 		       url-http-connection-opened
                        url-mime-accept-string
 		       url-http-proxy
@@ -1368,7 +1351,6 @@ The return value of this function is the retrieval buffer."
 	      url-callback-arguments cbargs
 	      url-http-after-change-function 'url-http-wait-for-headers-change-function
 	      url-http-target-url url-current-object
-	      url-http-no-retry retry-buffer
 	      url-http-connection-opened nil
               url-mime-accept-string mime-accept-string
 	      url-http-proxy url-using-proxy
@@ -1464,13 +1446,14 @@ The return value of this function is the retrieval buffer."
 (defun url-http-async-sentinel (proc why)
   ;; We are performing an asynchronous connection, and a status change
   ;; has occurred.
-  (when (buffer-name (process-buffer proc))
+  (when (buffer-live-p (process-buffer proc))
     (with-current-buffer (process-buffer proc)
       (cond
        (url-http-connection-opened
-	(setq url-http-no-retry t)
+        (url-http-debug "holy smoke: %s %s" (process-name proc) why)
 	(url-http-end-of-document-sentinel proc why))
        ((string= (substring why 0 4) "open")
+        (url-http-debug "crazy daisy: %s %s" (process-name proc) why)
 	(setq url-http-connection-opened t)
         (if (and url-http-proxy (string= "https" (url-type url-current-object)))
             (url-https-proxy-connect proc)
