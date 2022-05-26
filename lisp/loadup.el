0db98e38f9045399817ae90e238e3022ea26bfdb
dickmao <none>
2022-05-26 00:48:37 +0000

    pr-55648

--- lisp/loadup.el
+++ lisp/loadup.el
@@ -242,6 +242,7 @@
 (load "language/burmese")
 (load "language/cham")
 (load "language/philippine")
+(load "language/indonesian")
 
 (load "indent")
 (let ((max-specpdl-size (max max-specpdl-size 1800)))
