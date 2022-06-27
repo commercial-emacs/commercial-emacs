2f30ce373e5a3a44fbc5eabb111a2d009005ea71
dickmao <none>
2022-06-26 23:41:59 -0400

    Revert "LIEZ just hates traditional CI practices"
    This reverts commit c7cf8b6d5511bfbcdb95c1cc6220b9db7f4c7993.

--- test/lisp/emacs-lisp/lisp-tests.el
+++ test/lisp/emacs-lisp/lisp-tests.el
@@ -667,13 +667,13 @@ Second\n
       "
 \(defun foo ()
   \"Summary.
-=!inside=Testing keywords: 01234567890123456789\"
+=!inside=Testing keywords: :one :two :three\"
   (body))" ; FIXME: Remove parens around body to test Bug#28937 once it's fixed
     (goto-char inside)
     (let ((emacs-lisp-docstring-fill-column 30))
       (fill-paragraph))
-    (next-line)
-    (should (eq ?3 (char-after (point))))
+    (forward-line)
+    (should (looking-at ":three"))
     (end-of-line)
     (should-not (eq (preceding-char) ?\)))))
 
