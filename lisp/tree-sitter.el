c0a2587b85e48c1cddb03b6258b1fbae2ba0d46c
dickmao <none>
2022-08-11 10:03:41 -0400

    tree-sitter font-lock-mode-hook must be local.

--- lisp/tree-sitter.el
+++ lisp/tree-sitter.el
@@ -143,11 +143,11 @@ On Linux systems this is $XDG_CACHE_HOME/tree-sitter."
   (if tree-sitter-lock-mode
       (progn
         (setq-local font-lock-fontify-region-function #'tree-sitter-fontify-region)
-        (add-hook 'font-lock-mode-hook #'font-lock-ensure)
+        (add-hook 'font-lock-mode-hook #'font-lock-ensure nil t)
         (add-hook 'fontification-functions #'tree-sitter-do-fontify nil t)
         (add-hook 'after-change-functions #'tree-sitter-douse-fontify nil t))
     (kill-local-variable 'font-lock-fontify-region-function)
-    (remove-hook 'font-lock-mode-hook #'font-lock-ensure)
+    (remove-hook 'font-lock-mode-hook #'font-lock-ensure t)
     (remove-hook 'fontification-functions #'tree-sitter-do-fontify t)
     (remove-hook 'after-change-functions #'tree-sitter-douse-fontify t)))
 
