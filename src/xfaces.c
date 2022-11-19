1a94b762cb5bf783972261b03c8f2cb63efecc2e
dickmao <none>
2022-11-19 09:39:12 -0500

    Revert "Merge branch 'upstream'"
    This reverts commit 2a1fa68c58712bbf42c33148a32b8c107e29be43, reversing
    changes made to 9c5141818456fbf462e6ae780d6d197c4b23eb99.

--- src/xfaces.c
+++ src/xfaces.c
@@ -3796,12 +3796,8 @@ set_font_frame_param (Lisp_Object frame, Lisp_Object lface)
 	  ASET (lface, LFACE_FONT_INDEX, font);
 	}
       f->default_face_done_p = false;
-      AUTO_LIST2 (arg, AUTO_CONS_EXPR (Qfont, font),
-		  /* Clear the `font-parameter' frame property, as the
-		     font is now being specified by a face, not a
-		     frame property.  */
-		  AUTO_CONS_EXPR (Qfont_parameter, Qnil));
-      gui_set_frame_parameters_1 (f, arg, true);
+      AUTO_FRAME_ARG (arg, Qfont, font);
+      Fmodify_frame_parameters (frame, arg);
     }
 }
 
@@ -4198,17 +4194,7 @@ Default face attributes override any local face attributes.  */)
 	    {
 	      Lisp_Object name = newface->font->props[FONT_NAME_INDEX];
 	      AUTO_FRAME_ARG (arg, Qfont, name);
-
-#ifdef HAVE_WINDOW_SYSTEM
-	      if (FRAME_WINDOW_P (f))
-		/* This is a window-system frame.  Prevent changes of
-		   the `font' parameter here from messing with the
-		   `font-parameter' frame property, as the frame
-		   parameter is not being changed by the user.  */
-	        gui_set_frame_parameters_1 (f, arg, true);
-	      else
-#endif
-		Fmodify_frame_parameters (frame, arg);
+	      Fmodify_frame_parameters (frame, arg);
 	    }
 
 	  if (STRINGP (gvec[LFACE_FOREGROUND_INDEX]))
