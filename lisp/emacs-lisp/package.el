92a5b676edf5cc36e785d4536d2da2b4bc06fd61
Po Lu <luangruo@yahoo.com>
2022-12-01 14:24:43 +0800

    Merge from origin/emacs-29
    4161023a63 Fix crashes and memory leaks during display deinitialization
    785fa80159 New user option: project-vc-extra-root-markers
    7a1f1825fd Add inline to c-ts-mode--keywords
    3d976d6a03 Remove duplicate "friend" from c-ts-mode--keywords
    01e7d4b2a1 Make treesit-defun-prefer-top-level more flexible
    fb5d9ff559 Fix c-ts-mode defun movement (bug#59628)
    7397f2099b * lisp/tab-bar.el: Keep tabs of the same group together (b...
    832b013ef0 Insert news for installed packages only
    cc6efa7992 ; Fix type error in 'package-maintainers'
    ee32ae7c36 ; * etc/NEWS: Add missing tree-sitter based modes.
    33546d6dd2 ; * etc/NEWS: Move an entry to a proper place.  (bug#59718)
    8617edfffd ; Fix typos

--- lisp/emacs-lisp/package.el
+++ lisp/emacs-lisp/package.el
@@ -2622,7 +2622,13 @@ Helper function for `describe-package'."
          (incompatible-reason (package--incompatible-p desc))
          (signed (if desc (package-desc-signed desc)))
          (maintainer (cdr (assoc :maintainer extras)))
-         (authors (cdr (assoc :authors extras))))
+         (authors (cdr (assoc :authors extras)))
+         (news (and-let* (pkg-dir
+                          ((not built-in))
+                          (file (expand-file-name "news" pkg-dir))
+                          ((file-regular-p file))
+                          ((file-readable-p file)))
+                 file)))
     (when (string= status "avail-obso")
       (setq status "available obsolete"))
     (when incompatible-reason
