7757cc269b8de8c3c847c032812d57520aaa0d59
dickmao <none>
2023-01-16 16:46:17 -0500

    whitespace

--- src/sysdep.c
+++ src/sysdep.c
@@ -1711,7 +1711,7 @@ handle_signal (int sig, signal_handler_t handler)
 #ifdef HAVE_PTHREAD
   /* Propagate the signal to the main thread if necessary (POSIX
      admits the possibility of non-main threads receiving signals,
-     although that's (currently) verboten under GNU/Linux).  */
+     although that's currently verboten under GNU/Linux).  */
   if (! pthread_equal (pthread_self (), main_thread_id))
     {
       sigset_t blocked;
