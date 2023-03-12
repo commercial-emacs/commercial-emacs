62ad01eb6005bf491afb50d22457e0447e761658
dickmao <none>
2023-03-12 09:59:59 -0400

    fix shortdoc-function-examples-test

--- lisp/emacs-lisp/shortdoc.el
+++ lisp/emacs-lisp/shortdoc.el
@@ -1522,12 +1522,8 @@ function's documentation in the Info manual"))
     (insert "\n")
     (add-face-text-property start-section (point) 'shortdoc-section t)
     (let ((print-escape-newlines t)
-          (double-arrow (if (char-displayable-p ?⇒)
-                            "⇒"
-                          "=>"))
-          (single-arrow (if (char-displayable-p ?→)
-                            "→"
-                          "->"))
+          (double-arrow "⇒")
+          (single-arrow "→")
           (start-example (point)))
       (cl-loop for (type value) on data by #'cddr
                do
@@ -1634,15 +1630,9 @@ by \\[describe-function]."
                  (count 0)
                  (pos 0)
                  (end (length text))
-                 (double-arrow (if (char-displayable-p ?⇒)
-                                   "    ⇒"
-                                 "    =>"))
-                 (double-arrow-example (if (char-displayable-p ?⇒)
-                                           "    e.g. ⇒"
-                                         "    e.g. =>"))
-                 (single-arrow (if (char-displayable-p ?→)
-                                   "    →"
-                                 "    ->")))
+                 (double-arrow "    ⇒")
+                 (double-arrow-example "    e.g. ⇒")
+                 (single-arrow "    →"))
             (while (and (< pos end)
                         (or (string-match double-arrow text pos)
                             (string-match double-arrow-example text pos)
