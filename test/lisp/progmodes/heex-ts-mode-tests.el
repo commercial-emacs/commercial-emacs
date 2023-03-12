73192bbed89740cbf297c7e0480bb276bc91ff55
dickmao <none>
2023-03-12 17:10:03 -0400

    Revert "Merge branch 'upstream'"
    This reverts commit f60cf9442a955a7b62d94290a48e9e02fad5e8a6, reversing
    changes made to c0c69175dfd375f0c69efcc84f9fbbc036fab855.

--- test/lisp/progmodes/heex-ts-mode-tests.el
+++
@@ -1,9 +0,0 @@
-(require 'ert)
-(require 'ert-x)
-(require 'treesit)
-
-(ert-deftest heex-ts-mode-test-indentation ()
-  (skip-unless (treesit-ready-p 'heex))
-  (ert-test-erts-file (ert-resource-file "indent.erts")))
-
-(provide 'heex-ts-mode-tests)
