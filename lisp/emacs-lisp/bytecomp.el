6ea260aeddffa5b9fe48d99cd9ba64b4668cc4ed
Mattias Engdegård <mattiase@acm.org>
2023-05-30 17:09:45 +0200

    Add function declarations
    * lisp/emacs-lisp/byte-opt.el (side-effect-free-fns):
    Add get-byte, string-width, unibyte-string, special-variable-p, frexp,
    buffer-hash, buffer-line-statistics, load-average, md5, secure-hash,
    string-collate-equalp, string-collate-lessp, string-to-unibyte,
    string-version-lessp, current-cpu-time.
    (side-effect-and-error-free-fns): Add equal-including-properties.
    (pure-fns): Add equal-including-properties, string-version-lessp.
    * lisp/emacs-lisp/bytecomp.el (important-return-value-fns):
    Add match-data.
    * lisp/subr.el (buffer-local-boundp, subr-primitive-p, gensym)
    (frame-configuration-p, apply-partially, make-composed-keymap)
    (keymap-canonicalize, listify-key-sequence, event-modifiers)
    (event-basic-type, mouse-event-p, event-start, event-end)
    (event-click-count, event-line-count, posnp, posn-window, posn-area)
    (posn-point, posn-x-y, posn-col-row, posn-actual-col-row)
    (posn-timestamp, posn-string, posn-image, posn-object)
    (posn-object-x-y, posn-object-width-height, provided-mode-derived-p)
    (derived-mode-p, autoloadp, locate-eln-file, symbol-file)
    (process-lines-handling-status, process-lines)
    (process-lines-ignore-status, process-get)
    (copy-overlay, shell-quote-argument, field-at-pos):
    Add appropriate function declarations.

--- lisp/emacs-lisp/bytecomp.el
+++ lisp/emacs-lisp/bytecomp.el
@@ -3346,6 +3346,8 @@ value should be byte-discard."
          ;; when used on arrays, but most calls pass lists.
          nreverse sort
 
+         match-data
+
          ;; Adding these functions causes many warnings;
          ;; evaluate how many of them are false first.
          delq delete
