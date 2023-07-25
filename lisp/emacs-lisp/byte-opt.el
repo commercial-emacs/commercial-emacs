0967afe9893a4c35771cfd26e25062bd1db2ee73
Mattias Engdegård <mattiase@acm.org>
2023-07-25 12:52:29 +0200

    * lisp/emacs-lisp/byte-opt.el (pure-fns): Add `max-char`

--- lisp/emacs-lisp/byte-opt.el
+++ lisp/emacs-lisp/byte-opt.el
@@ -1920,7 +1920,7 @@ See Info node `(elisp) Integer Basics'."
 (let ((pure-fns
        '(
          ;; character.c
-         characterp
+         characterp max-char
          ;; data.c
          % * + - / /= 1+ 1- < <= = > >= aref arrayp ash atom bare-symbol
          bool-vector-count-consecutive bool-vector-count-population
