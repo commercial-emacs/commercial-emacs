fa47dc066ad4c7849b0bb657f7fbe04690b3fc81
dickmao <none>
2023-09-01 05:42:25 -0400

    Revert "Merge branch 'upstream'"
    This reverts commit 37b51b56a79545148a6c128e4d1483474f3aee73, reversing
    changes made to c37eb5267e10e9a459750d247fde8512fd42dc34.

--- lisp/eshell/eshell.el
+++ lisp/eshell/eshell.el
@@ -301,8 +301,7 @@ argument), then insert output into the current buffer at point."
                     `(let ((eshell-current-handles
                             (eshell-create-handles ,stdout 'insert))
                            (eshell-current-subjob-p))
-		       ,(eshell-parse-command command))
-                    command))
+		       ,(eshell-parse-command command))))
 	     intr
 	     (bufname (if (eq (car-safe proc) :eshell-background)
 			  "*Eshell Async Command Output*"
