3ba89115aca5569d8ea39ca8c36e857130249669
Stefan Monnier <monnier@iro.umontreal.ca>
2024-01-18 14:05:16 -0500

    * lisp/progmodes/elisp-mode.el (elisp-completion-at-point): Fix bug#68514
    Redo the commit 0db2126d7176 to try and avoid selecting more than
    a mere symbol.

--- lisp/progmodes/elisp-mode.el
+++ lisp/progmodes/elisp-mode.el
@@ -655,12 +655,13 @@ functions are annotated with \"<f>\" via the
 		    (save-excursion
 		      (backward-sexp 1)
 		      (skip-chars-forward "`',‘#")
-		      (point))
+		      (min (point) pos))
 		  (scan-error pos)))
 	   (end
-	    (when (and (< beg (point-max))
-		       (not (memq (char-syntax (char-after beg))
-                                  '(?\" ?\())))
+	    (cond
+	     ((and (< beg (point-max))
+		   (memq (char-syntax (char-after beg))
+		                   '(?w ?\\ ?_)))
 	      (condition-case nil
 		  (save-excursion
 		    (goto-char beg)
@@ -668,7 +669,11 @@ functions are annotated with \"<f>\" via the
                     (skip-chars-backward "'’")
 		    (when (>= (point) pos)
 		      (point)))
-		(scan-error pos))))
+		(scan-error pos)))
+             ((or (>= beg (point-max))
+                  (memq (char-syntax (char-after beg))
+		        '(?\) ?\s)))
+              beg)))
            ;; t if in function position.
            (funpos (eq (char-before beg) ?\())
            (quoted (elisp--form-quoted-p beg))
