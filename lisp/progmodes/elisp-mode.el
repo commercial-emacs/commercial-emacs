6133bb6ea4b00e5c52b90fcd23fdc9d715aecb81
Stefan Monnier <monnier@iro.umontreal.ca>
2024-02-24 17:52:14 -0500

    * lisp/progmodes/elisp-mode.el (eval-last-sexp, eval-defun): Fix thinko

--- lisp/progmodes/elisp-mode.el
+++ lisp/progmodes/elisp-mode.el
@@ -1615,7 +1615,8 @@ If `eval-expression-debug-on-error' is non-nil, which is the default,
 this command arranges for all errors to enter the debugger."
   (interactive "P")
   (values--store-value
-   (handler-bind ((error (if #'eval-expression--debug #'ignore)))
+   (handler-bind ((error (if eval-expression-debug-on-error
+                             #'eval-expression--debug #'ignore)))
      (elisp--eval-last-sexp eval-last-sexp-arg-internal))))
 
 (defun elisp--eval-defun-1 (form)
@@ -1754,7 +1755,8 @@ which see."
 	 (defvar edebug-all-defs)
 	 (eval-defun (not edebug-all-defs)))
 	(t
-	 (handler-bind ((error (if #'eval-expression--debug #'ignore)))
+	 (handler-bind ((error (if eval-expression-debug-on-error
+	                           #'eval-expression--debug #'ignore)))
 	   (elisp--eval-defun)))))
 
 ;;; ElDoc Support
