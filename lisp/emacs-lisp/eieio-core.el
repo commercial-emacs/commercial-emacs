72c0dcb4c440ef96b97afec9a59ed40155aa5670
dickmao <none>
2024-03-08 15:31:38 -0500

    Revert "Merge branch 'upstream'"
    This reverts commit b9cd39cfa24f809f0dec5c18aa2bbdc75c471578, reversing
    changes made to 1ed457c7a2fdd8c7b108a5e76acb67a7d57e355b.

--- lisp/emacs-lisp/eieio-core.el
+++ lisp/emacs-lisp/eieio-core.el
@@ -1066,7 +1066,8 @@ method invocation orders of the involved classes."
   (lambda (tag &rest _)
     (let ((class (cl--find-class tag)))
       (and (eieio--class-p class)
-           (cl--class-allparents class)))))
+           (mapcar #'eieio--class-name
+                   (eieio--class-precedence-list class))))))
 
 (cl-defmethod cl-generic-generalizers :extra "class" (specializer)
   "Support for dispatch on types defined by EIEIO's `defclass'."
@@ -1088,9 +1089,10 @@ method invocation orders of the involved classes."
 ;; Instead, we add a new "subclass" specializer.
 
 (defun eieio--generic-subclass-specializers (tag &rest _)
-  (when (cl--class-p tag)
-    (mapcar (lambda (class) `(subclass ,class))
-            (cl--class-allparents tag))))
+  (when (eieio--class-p tag)
+    (mapcar (lambda (class)
+              `(subclass ,(eieio--class-name class)))
+            (eieio--class-precedence-list tag))))
 
 (cl-generic-define-generalizer eieio--generic-subclass-generalizer
   60 (lambda (name &rest _) `(and (symbolp ,name) (cl--find-class ,name)))
