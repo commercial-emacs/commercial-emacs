4ba5cfb40b024bf1361c4eb66a5294976acfa6f9
dickmao <none>
2024-03-15 19:35:49 -0400

    Revert "Merge branch 'upstream'"
    This reverts commit 725c0caf6dce6eaed538a8be6e7e8d9a38d96973, reversing
    changes made to a12f68364513562772906bd632fd9850154667c2.

--- lisp/gnus/gnus-notifications.el
+++ lisp/gnus/gnus-notifications.el
@@ -85,33 +85,22 @@ not get notifications."
 (defun gnus-notifications-notify (from subject photo-file)
   "Send a notification about a new mail.
 Return a notification id if any, or t on success."
-  (if (featurep 'android)
+  (if (fboundp 'notifications-notify)
       (gnus-funcall-no-warning
-       'android-notifications-notify
+       'notifications-notify
        :title from
        :body subject
        :actions '("read" "Read" "mark-read" "Mark As Read")
        :on-action 'gnus-notifications-action
-       :on-close 'gnus-notifications-close
-       :group "Email arrivals"
+       :app-icon (gnus-funcall-no-warning
+                  'image-search-load-path "gnus/gnus.png")
+       :image-path photo-file
+       :app-name "Gnus"
+       :category "email.arrived"
        :timeout gnus-notifications-timeout)
-    (if (fboundp 'notifications-notify)
-        (gnus-funcall-no-warning
-         'notifications-notify
-         :title from
-         :body subject
-         :actions '("read" "Read" "mark-read" "Mark As Read")
-         :on-action 'gnus-notifications-action
-         :on-close 'gnus-notifications-close
-         :app-icon (gnus-funcall-no-warning
-                    'image-search-load-path "gnus/gnus.png")
-         :image-path photo-file
-         :app-name "Gnus"
-         :category "email.arrived"
-         :timeout gnus-notifications-timeout)
-      (message "New message from %s: %s" from subject)
-      ;; Don't return an id
-      t)))
+    (message "New message from %s: %s" from subject)
+    ;; Don't return an id
+    t))
 
 (declare-function gravatar-retrieve-synchronously "gravatar.el"
 		  (mail-address))
