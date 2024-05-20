0352c9a933aa9fb01a66962588ebaa3b9529f4e3
Jim Porter <jporterbugs@gmail.com>
2024-05-20 12:37:22 -0700

    Improve implementation of 'forward-thing' using custom providers
    Now, call all the custom providers for each step, using the provider
    that moves point the smallest non-zero amount.  This allows multiple
    providers for a given "thing" to work nicely together.
    
    * lisp/thingatpt.el (forward-thing-provider-alist): Update docstring.
    (forward-thing): New implementation to call each provider N times.
    (forward-thing-for-text-property): Take BACKWARD instead of N.  Update
    callers.
    
    * test/lisp/thingatpt-tests.el (thing-at-point-providers)
    (forward-thing-providers): Add more checks.
    (consecutive-things-at-point): New test.

--- lisp/net/eww.el
+++ lisp/net/eww.el
@@ -1358,9 +1358,9 @@ This consults the entries in `eww-readable-urls' (which see)."
   "`thing-at-point' provider function."
   (thing-at-point-for-text-property 'shr-url))
 
-(defun eww--forward-url (n)
+(defun eww--forward-url (backward)
   "`forward-thing' provider function."
-  (forward-thing-for-text-property 'shr-url n))
+  (forward-thing-for-text-property 'shr-url backward))
 
 (defun eww--bounds-of-url-at-point ()
   "`bounds-of-thing-at-point' provider function."
