cd41ff484ff39235d5936c51e580458614ae820d
Sean Whitton <spwhitton@spwhitton.name>
2024-05-30 12:45:57 +0100

    universal-argument--preserve: Preserve last-prefix-arg
    * lisp/simple.el (universal-argument--preserve): Set
    current-prefix-arg to last-prefix-arg in order to preserve
    last-prefix-arg, too (bug#71277).

--- lisp/simple.el
+++ lisp/simple.el
@@ -5405,7 +5405,8 @@ Runs `prefix-command-preserve-state-hook'."
 (add-hook 'prefix-command-preserve-state-hook
           #'universal-argument--preserve)
 (defun universal-argument--preserve ()
-  (setq prefix-arg current-prefix-arg))
+  (setq prefix-arg current-prefix-arg)
+  (setq current-prefix-arg last-prefix-arg))
 
 (defvar universal-argument-map
   (let ((map (make-sparse-keymap))
