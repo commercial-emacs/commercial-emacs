352a8b0f93e719f2f0dfcf601cd00461090f8d31
Jim Porter <jporterbugs@gmail.com>
2024-05-29 19:21:09 -0700

    Make Eshell's "which" command extensible
    Since 'eshell-named-command-hook' already makes execution of commands
    extensible, "which" should be too.  This makes sure that "which" returns
    the right result for quoted commands like "/:cat".
    
    * lisp/eshell/em-alias.el (eshell-aliases-file): Allow it to be nil.
    (eshell-read-aliases-list, eshell-write-aliases-list): Check if
    'eshell-aliases-file' is nil.
    (eshell-maybe-replace-by-alias--which): New function...
    (eshell-maybe-replace-by-alias): ... use it.
    
    * lisp/eshell/esh-cmd.el (eshell-named-command-hook): Update docstring.
    (eshell/which): Make extensible.
    (eshell--find-plain-lisp-command, eshell-plain-command--which): New
    functions.
    (eshell-plain-command): Use 'eshell--find-plain-lisp-command'.
    
    * lisp/eshell/esh-ext.el (eshell-explicit-command--which): New
    function...
    (eshell-explicit-command): ... unise it.
    (eshell-quoted-file-command--which): New function...
    (eshell-quoted-file-command): ... use it.
    (eshell-external-command--which): New function.
    
    * test/lisp/eshell/esh-cmd-tests.el
    (esh-cmd-test/which/plain/eshell-builtin)
    (esh-cmd-test/which/plain/external-program)
    (esh-cmd-test/which/plain/not-found, esh-cmd-test/which/alias)
    (esh-cmd-test/which/explicit, esh-cmd-test/which/explicit/not-found)
    (esh-cmd-test/which/quoted-file)
    (esh-cmd-test/which/quoted-file/not-found): New tests.
    
    * test/lisp/eshell/eshell-tests-helpers.el (with-temp-eshell-settings):
    Don't load or save aliases.
    (eshell-command-result--match,eshell-command-result--match-explainer)
    (eshell-command-result-match): New functions.

--- lisp/eshell/esh-ext.el
+++ lisp/eshell/esh-ext.el
@@ -186,6 +186,11 @@ commands on your local host by using the \"/local:\" prefix, like
   (add-hook 'eshell-named-command-hook #'eshell-quoted-file-command nil t)
   (add-hook 'eshell-named-command-hook #'eshell-explicit-command nil t))
 
+(defun eshell-explicit-command--which (command)
+  (when (and (> (length command) 1)
+             (eq (aref command 0) eshell-explicit-command-char))
+    (eshell-external-command--which (substring command 1))))
+
 (defun eshell-explicit-command (command args)
   "If a command name begins with \"*\", always call it externally.
 This bypasses all Lisp functions and aliases."
@@ -198,12 +203,22 @@ This bypasses all Lisp functions and aliases."
 	(error "%s: external command not found"
 	       (substring command 1))))))
 
+(put 'eshell-explicit-command 'eshell-which-function
+     #'eshell-explicit-command--which)
+
+(defun eshell-quoted-file-command--which (command)
+  (when (file-name-quoted-p command)
+    (eshell-external-command--which (file-name-unquote command))))
+
 (defun eshell-quoted-file-command (command args)
   "If a command name begins with \"/:\", always call it externally.
 Similar to `eshell-explicit-command', this bypasses all Lisp functions
 and aliases, but it also ignores file name handlers."
   (when (file-name-quoted-p command)
     (eshell-external-command (file-name-unquote command) args)))
+
+(put 'eshell-quoted-file-command 'eshell-which-function
+     #'eshell-quoted-file-command--which)
 
 (defun eshell-remote-command (command args)
   "Insert output from a remote COMMAND, using ARGS.
@@ -244,6 +259,11 @@ current working directory."
       (declare-function eshell-gather-process-output "esh-proc" (command args))
       (eshell-gather-process-output
        (car interp) (append (cdr interp) args)))))
+
+(defun eshell-external-command--which (command)
+  (or (eshell-search-path command)
+      (error "no %s in (%s)" command
+             (string-join (eshell-get-path t) (path-separator)))))
 
 (defun eshell-external-command (command args)
   "Insert output from an external COMMAND, using ARGS."
