0369f1745202c9f2e9c11c70cfcf70b2ab19a612
Collin Funk <collin.funk1@gmail.com>
2024-06-03 02:35:06 -0700

    ; * src/xterm.c (syms_of_xterm): Fix doc string typos.
    Bug#71333
    
    Copyright-paperwork-exempt: yes

--- src/xterm.c
+++ src/xterm.c
@@ -32056,7 +32056,7 @@ With MS Windows, Haiku windowing or Nextstep, the value is t.  */);
   DEFSYM (Qreally_fast, "really-fast");
 
   DEFVAR_LISP ("x-ctrl-keysym", Vx_ctrl_keysym,
-    doc: /* Which modifer value Emacs reports when Ctrl is depressed.
+    doc: /* Which modifier value Emacs reports when Ctrl is depressed.
 This should be one of the symbols `ctrl', `alt', `hyper', `meta', or
 `super', representing a modifier to be reported for key events with the
 Ctrl modifier (i.e. the keysym Ctrl_L or Ctrl_R) depressed, with nil or
@@ -32064,7 +32064,7 @@ any other value equivalent to `ctrl'.  */);
   Vx_ctrl_keysym = Qnil;
 
   DEFVAR_LISP ("x-alt-keysym", Vx_alt_keysym,
-    doc: /* Which modifer value Emacs reports when Alt is depressed.
+    doc: /* Which modifier value Emacs reports when Alt is depressed.
 This should be one of the symbols `ctrl', `alt', `hyper', `meta', or
 `super', representing a modifier to be reported for key events with the
 Alt modifier (e.g. the keysym Alt_L or Alt_R, if the keyboard features a
@@ -32073,7 +32073,7 @@ equivalent to `alt'.  */);
   Vx_alt_keysym = Qnil;
 
   DEFVAR_LISP ("x-hyper-keysym", Vx_hyper_keysym,
-    doc: /* Which modifer value Emacs reports when Hyper is depressed.
+    doc: /* Which modifier value Emacs reports when Hyper is depressed.
 This should be one of the symbols `ctrl', `alt', `hyper', `meta', or
 `super', representing a modifier to be reported for key events with the
 Hyper modifier (i.e. the keysym Hyper_L or Hyper_R) depressed, with nil
@@ -32081,7 +32081,7 @@ or any other value equivalent to `hyper'.  */);
   Vx_hyper_keysym = Qnil;
 
   DEFVAR_LISP ("x-meta-keysym", Vx_meta_keysym,
-    doc: /* Which modifer value Emacs reports when Meta is depressed.
+    doc: /* Which modifier value Emacs reports when Meta is depressed.
 This should be one of the symbols `ctrl', `alt', `hyper', `meta', or
 `super', representing a modifier to be reported for key events with the
 Meta modifier (e.g. the keysym Alt_L or Alt_R, when the keyboard does
@@ -32090,7 +32090,7 @@ value equivalent to `meta'.  */);
   Vx_meta_keysym = Qnil;
 
   DEFVAR_LISP ("x-super-keysym", Vx_super_keysym,
-    doc: /* Which modifer value Emacs reports when Super is depressed.
+    doc: /* Which modifier value Emacs reports when Super is depressed.
 This should be one of the symbols `ctrl', `alt', `hyper', `meta', or
 `super', representing a modifier to be reported for key events with the
 Super modifier (i.e. the keysym Super_L or Super_R) depressed, with nil
