f9d4156697c997090f626799ed08da7a62584e4b
Eli Zaretskii <eliz@gnu.org>
2024-06-09 10:56:39 +0300

    Fix Eshell tests on MS-Windows
    * lisp/eshell/esh-util.el (eshell-get-path): Don't add "." if it
    is already there.
    
    * test/lisp/eshell/esh-var-tests.el
    (esh-var-test/path-var/preserve-across-hosts): Skip on MS-Windows.
    (esh-var-test/path-var/set, esh-var-test/path-var/set-locally):
    Quote the PATH value, for MS-Windows.
    * test/lisp/eshell/esh-util-tests.el (esh-util-test/path/get): No
    need to add ".": it is already done by 'eshell-get-path'.
    * test/lisp/eshell/esh-proc-tests.el
    (esh-proc-test/kill-pipeline): Accept empty string as valid
    output.
    (esh-proc-test/sigpipe-exits-process): Skip on MS-Windows: no
    SIGPIPE.
    (esh-proc-test/emacs-command): Quote correctly for MS-Windows.
    * test/lisp/eshell/em-unix-tests.el
    (em-unix-test/compile/interactive): Fix test on MS-Windows.
    * test/lisp/eshell/em-script-tests.el (em-script-test/batch-file):
    Skip on MS-Windows.
    * test/lisp/eshell/eshell-tests-helpers.el
    (eshell-command-result--equal): Compare strings (file names)
    case-insensitively on MS-Windows.

--- test/lisp/eshell/eshell-tests-helpers.el
+++ test/lisp/eshell/eshell-tests-helpers.el
@@ -186,7 +186,8 @@ inserting the command."
 (defun eshell-command-result--equal (_command actual expected)
   "Compare the ACTUAL result of a COMMAND with its EXPECTED value."
   (or (equal actual expected)
-      (when (and (eq system-type 'darwin) (stringp expected))
+      (when (and (memq system-type '(windows-nt ms-dos darwin))
+		 (stringp expected))
         (equal actual (concat expected "\n")))
       (not (zerop (1+ (cl-decf eshell-tests-max-errors))))))
 
