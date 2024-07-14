emacs-mirror/emacs

Remove unnecessary cons in function-type property
* lisp/emacs-lisp/comp-common.el (comp-function-type-spec): Remove
unnecessary car.
* lisp/emacs-lisp/comp.el (comp--intern-func-in-ctxt): Likewise.
* lisp/emacs-lisp/byte-run.el (byte-run--set-function-type): Update
lambda list.

lisp/emacs-lisp/comp-common.el
@@ -517,7 +517,7 @@ itself."
       (when (and f (null type-spec))
         (if-let ((delc-type (function-get function 'function-type)))
             ;; Declared Lisp function
-            (setf type-spec (car delc-type))
+            (setf type-spec delc-type)
           (when (subr-native-elisp-p f)
             ;; Native compiled inferred
             (setf kind 'inferred

commercial-emacs/commercial-emacs

lisp/emacs-lisp/comp-common.el
@@ -488,7 +488,7 @@ compiler) or 'declared (by the function type itself or is one of
     (when (and f (null type-spec))
       (if-let ((decl-type (function-get function 'function-type)))
           ;; Declared Lisp function
-          (setf type-spec (car decl-type))
+          (setf type-spec decl-type)
         (when (subr-native-elisp-p f)
           ;; Native compiled inferred
           (setf kind 'inferred