5f633a56a019f2de9794e6b7582475577e3c6e77
Paul Eggert <eggert@cs.ucla.edu>
2024-08-16 17:20:58 -0700

    Don’t ignore -Wclobbered in keyboard.c
    This fixes Emacs bug#71744.
    * src/keyboard.c: Do not ignore -Wclobbered.
    (read_char): Fix violation of the C standard, where setjmp
    clobbered c.  Fix suggested by Pip Cet in
    <https://debbugs.gnu.org/cgi/bugreport.cgi?bug=71744#38>

--- src/keyboard.c
+++ src/keyboard.c
@@ -75,11 +75,6 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 #include TERM_HEADER
 #endif /* HAVE_WINDOW_SYSTEM */
 
-/* Work around GCC bug 54561.  */
-#if GNUC_PREREQ (4, 3, 0)
-# pragma GCC diagnostic ignored "-Wclobbered"
-#endif
-
 #ifdef WINDOWSNT
 char const DEV_TTY[] = "CONOUT$";
 #else
@@ -2233,7 +2228,7 @@ read_char (int commandflag, Lisp_Object map,
 	   Lisp_Object prev_event,
 	   bool *used_mouse_menu, struct timespec *end_time)
 {
-  Lisp_Object c = Qnil;
+  Lisp_Object volatile c = Qnil;
   sys_jmp_buf local_getcjmp;
   sys_jmp_buf save_jump;
   Lisp_Object tem, save;
