879a5d7d00b1d56b5ccfdddc9b1391f23e122ad3
Alan Mackenzie <acm@muc.de>
2024-09-24 20:45:29 +0000

    CC Mode: Separate indentation of enums from that of brace lists
    This will allow enums to be indented as in the GNU coding
    standards without affecting brace lists.
    
    * lisp/progmodes/cc-align.el
    (c-lineup-arglist-intro-after-paren)
    (c-lineup-arglist-close-under-paren)
    (c-lineup-whitesmith-in-block)
    (c-lineup-after-whitesmith-blocks): Amend doc strings to
    indicate they can be used for enum-* syntactic symbols too.
    
    * lisp/progmodes/cc-engine.el (c-looking-at-decl-block)
    (c-backward-typed-enum-colon, c-backward-over-enum-header):
    Rename c-after-brace-list-key to c-after-enum-list-key and
    c-brace-list-key to c-enum-list-key.
    (c-at-enum-brace): New function.
    (c-inside-bracelist-p): Don't check for enums any more.
    (c-add-stmt-syntax): New cond arm for enums.
    (c-guess-continued-construct CASE B.6) New code for enum-open.
    (c-guess-basic-syntax CASE 5A.3, CASE 7B, CASE 9, CASE 9B)
    (CASE 9C, CASE 9D): Use the new function c-at-enum-brace, and
    use enum-* syntactic symbols rather than brace-list-* ones
    where appropriate.
    (c-used-syntactic-symbols): New dynamic variable.
    (c-evaluate-offset): Add handling for a different syntactic
    symbol as the cdr of a c-offsets-alist element.
    
    * lisp/progmodes/cc-fonts.el (c-get-fontification-context):
    Add a call to c-at-enum-brace since c-inside-bracelist-p no
    longer handles enums.
    (c-font-lock-enum-body, c-font-lock-enum-tail): Condense calls
    to c-backward-over-enum-header to c-at-enum-brace.
    (c-basic-matchers-after): Replace a c-brace-list-decl-kwds with
    c-enum-list-kwds.
    
    * lisp/progmodes/cc-langs.el (c-enum-list-kwds)
    (c-enum-list-key): New names for c-brace-list-decl-kwds and
    c-brace-list-key.
    (c-after-enum-list-kwds, c-after-enum-list-key): New names for
    c-after-brace-list-decl-kwds and c-after-brace-list-key.
    (c-enum-list-key, c-after-enum-list-key): Add code to
    use a value of an old name, should a derived mode still use
    that.
    (c-defun-type-name-decl-kwds, c-typedef-decl-kwds)
    (c-typeless-decl-kwds, c-prefix-spec-kwds)
    (c-brace-id-list-kwds, c-enum-clause-introduction-re): Use
    c-enum-list-kwds rather than c-brace-list-decl-kwds.
    
    * lisp/progmodes/cc-styles.el (c-read-offset): Add "a
    syntactic symbol" into an error message text.
    
    * lisp/progmodes/cc-vars.el (c-valid-offset): Allow for a
    syntactic symbol as the parameter.
    (c-offsets-alist): Add new syntactic symbols enum-open,
    enum-close, enum-intro, and enum-entry.
    
    * doc/misc/cc-mode.texi (Syntactic Symbols): Add a note that
    anchor points are briefly documented in cc-vars.el.  Correct
    the suffix "-block-intro" to "-intro".  Remove "an enum or"
    from the descriptions of the brace-list-* syntactic symbols.
    Add in new descriptions for the enum-* syntactic symbols.
    (Brace List Symbols): Remove references to enums.  Add a note
    that enums are no longer brace-lists.
    (Enum Symbols): A new subsection documenting the new enum-*
    syntactic symbols.
    (c-offsets-alist): Document the new possibility, syntactic
    symbol, for the cdr of an alist entry.
    (Brace/Paren Line-Up, List Line-Up): Note that the new enum
    syntactic symbols can be handled by these lineup functions.

--- lisp/progmodes/cc-fonts.el
+++ lisp/progmodes/cc-fonts.el
@@ -1279,11 +1279,12 @@ casts and declarations are fontified.  Used on level 2 and higher."
       (c-put-char-property (1- match-pos) 'c-type
 			   'c-decl-arg-start)
       (cons 'decl nil))
-     ;; We're inside a brace list.
+     ;; We're inside a brace list/enum list.
      ((and (eq (char-before match-pos) ?{)
-	   (c-inside-bracelist-p (1- match-pos)
-				 (cdr (c-parse-state))
-				 nil))
+	   (or (c-at-enum-brace (1- match-pos))
+	       (c-inside-bracelist-p (1- match-pos)
+				     (cdr (c-parse-state))
+				     nil)))
       (c-put-char-property (1- match-pos) 'c-type
 			   'c-not-decl)
       (cons 'not-decl nil))
@@ -1755,9 +1756,7 @@ casts and declarations are fontified.  Used on level 2 and higher."
   ;; Fontification".
   (while (and (< (point) limit)
 	      (search-forward-regexp c-enum-clause-introduction-re limit t))
-    (when (save-excursion
-	    (backward-char)
-	    (c-backward-over-enum-header))
+    (when (c-at-enum-brace (1- (point)))
       (c-forward-syntactic-ws)
       (c-font-lock-declarators limit t nil t)))
   nil)
@@ -1781,9 +1780,7 @@ casts and declarations are fontified.  Used on level 2 and higher."
       (when (and
 	     encl-pos
 	     (eq (char-after encl-pos) ?\{)
-	     (save-excursion
-	       (goto-char encl-pos)
-	       (c-backward-over-enum-header)))
+	     (c-at-enum-brace encl-pos))
 	(c-syntactic-skip-backward "^{," nil t)
 	(c-put-char-property (1- (point)) 'c-type 'c-decl-id-start)
 
@@ -2466,7 +2463,7 @@ on level 2 only and so aren't combined with `c-complex-decl-matchers'."
 generic casts and declarations are fontified.  Used on level 2 and
 higher."
 
-  t `(,@(when (c-lang-const c-brace-list-decl-kwds)
+  t `(,@(when (c-lang-const c-enum-list-kwds)
       ;; Fontify the remaining identifiers inside an enum list when we start
       ;; inside it.
 	  '(c-font-lock-enum-tail
