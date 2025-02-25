1450d84a08470bbcb7cd1dfe38dfc76a195e852f
dickmao <none>
2025-02-25 16:09:57 -0500

    undo--redo-p?

--- lisp/menu-bar.el
+++ lisp/menu-bar.el
@@ -581,12 +581,6 @@
     (if (featurep 'ns)
         (bindings--define-key menu [separator-undo] menu-bar-separator))
 
-    (bindings--define-key menu [undo-redo]
-      '(menu-item "Redo" undo-redo
-                  :enable (and (not buffer-read-only)
-                           (undo--redo-p buffer-undo-list))
-                  :help "Redo last undone edits"))
-
     (bindings--define-key menu [undo]
       '(menu-item "Undo" undo
                   :enable (and (not buffer-read-only)
